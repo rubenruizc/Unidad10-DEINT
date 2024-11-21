875c24bd9e0238d94752145404b169e156fd69a2
rruiz <rruiz@107-28>
2024-11-21 14:39:01 +0100

    Cosas Cambiadas

---
+++ DAL/ClsConexion.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class clsConexion
+    {
+        public static SqlConnection getConexion()
+        {
+            SqlConnection miConexion = new SqlConnection();
+
+            try
+            {
+
+                miConexion.ConnectionString = "server=auri.database.windows.net;database=AuriDB;uid=usuario;pwd=LaCampana123;trustServerCertificate = true;";
+
+                miConexion.Open();
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return miConexion;
+        }
+
+
+    }
+}
